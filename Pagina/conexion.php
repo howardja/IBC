dcb81f2523b71e19c18d894f4ea7361ae4159d82
howardja <howard_ja@hotmail.com>
2016-12-02 00:12:31 -0400

    CLEAR

--- Pagina/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$servidor = 'localhost';
-$usuario = 'ibccocha_admin';
-$password = 'IBv8SekUFSw0W*';
-$base_datos = 'ibccocha_sistema';
-$conexion = mysql_connect($servidor,$usuario,$password,$base_datos) or die (mysql_error());
-mysql_select_db($base_datos,$conexion);
-?>
--- Pagina/logout.php
+++
@@ -1,8 +0,0 @@
-<? 
-//Reanudamos la sesión 
-session_start(); 
-//Literalmente la destruimos 
-session_destroy(); 
-//Redireccionamos a index.html (al inicio de sesión) 
-header("Location: index.html"); 
-?>
